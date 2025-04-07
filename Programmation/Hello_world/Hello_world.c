faa6b2b9681771a66c6260d6cc8a7cb3346520fa
Pauloux <54153283+Pauloux@users.noreply.github.com>
2025-04-07 10:19:40 +0200

    Correction du programme Blink en Hello_world

---
+++ Programmation/Hello_world/Hello_world.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+
+#include "VirtualSerial.h"
+#include <util/delay.h>
+
+extern USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface;
+extern FILE USBSerialStream;
+
+int main(void) {
+	// Initialisation USB
+	SetupHardware();
+	CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
+	GlobalInterruptEnable();
+
+	// Déclaration
+    DDRF |= (1 << PORTF1);
+
+	while (1) {
+		PORTF ^= (1 << PORTF1);
+
+		fprintf(&USBSerialStream,"Hello world\n\r");
+
+		//   les 3 lignes ci-dessous pour accepter les signaux venant du PC 
+		CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+		CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
+		USB_USBTask();
+
+		_delay_ms(1000);
+	}
+}
