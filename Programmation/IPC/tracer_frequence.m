39ed47444c211c9fce2f14d74116731a2b53cf8b
KhalilFalah <khalilfalah37@gmail.com>
2025-04-08 16:15:18 +0200

    Ajout des légendes des axes pour les graphes

--- Programmation/IPC/tracer_frequence.m
+++ Programmation/IPC/tracer_frequence.m
@@ -1,7 +1,12 @@
 data = readmatrix('frequence.dat');
 
 nbr_cycle_ICP = data(:,1);
-nbr_clk_ICP = data(:,2);
+nbr_clk_ICP   = data(:,2);
 
 figure;
-plot(nbr_clk_ICP);
+plot(nbr_clk_ICP, 'LineWidth', 1.5);  
+title('Fréquence');
+xlabel('Temps (s)');
+ylabel('Valeur ICP');  
+grid on;          
+axis tight;       % Ajuste l'affichage autour des données
--- Programmation/Influence_Varicap/tracer_frequence.m
+++ Programmation/Influence_Varicap/tracer_frequence.m
@@ -1,15 +1,25 @@
 data = readmatrix('frequence.dat');
 
-duty_cycle = data(:,1);
+duty_cycle    = data(:,1);
 nbr_cycle_ICP = data(:,2);
-nbr_clk_ICP = data(:,3);
+nbr_clk_ICP   = data(:,3);
 
 figure;
 
+% Duty cycle
 subplot(2, 1, 1);
-plot(duty_cycle);
+plot(duty_cycle/1024*100, 'LineWidth', 1.5);
 title('Duty cycle');
+xlabel('Temps (s)');
+ylabel('Duty cycle (%)');
+grid on;         
+axis tight;      % Ajuste l'échelle automatiquement autour des valeurs de duty_cycle
 
+% Fréquence
 subplot(2, 1, 2);
-plot(nbr_clk_ICP);
-title('Fréquence')
+plot(nbr_clk_ICP, 'LineWidth', 1.5);
+title('Fréquence');
+xlabel('Temps (s)');
+ylabel('Valeur ICP');   
+grid on;             
+axis tight;               % Ajuste l'échelle automatiquement autour des valeurs de nbr_clk_ICP
