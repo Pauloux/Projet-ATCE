aa05cf91f09052ae96e8dcb6394c4e192d1db370
Pauloux <54153283+Pauloux@users.noreply.github.com>
2025-04-13 21:30:51 +0200

    Rédaction de la programmation de l'ICP

--- Programmation/IPC/tracer_frequence.m
+++ Programmation/IPC/tracer_frequence.m
@@ -4,9 +4,9 @@ nbr_cycle_ICP = data(:,1);
 nbr_clk_ICP   = data(:,2);
 
 figure;
-plot(nbr_clk_ICP, 'LineWidth', 1.5);  
-title('Fréquence');
+plot(nbr_clk_ICP, 'LineWidth', 1.5);
+title("Évolution de la valeur de l'ICP (ou de la fréquence % 65536 en Hz) en fonction du temps");
 xlabel('Temps (s)');
-ylabel('Valeur ICP');  
-grid on;          
-axis tight;       % Ajuste l'affichage autour des données
+ylabel('Valeur ICP ou fréquence % 65536 (Hz)');
+grid on;
+axis tight;
