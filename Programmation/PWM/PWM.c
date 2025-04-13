749b9031e967149533351ca0ca2f4ee2ceab59e8
Pauloux <54153283+Pauloux@users.noreply.github.com>
2025-04-13 16:52:19 +0200

    Modification des commentaires dans le programme du PWM

--- Programmation/PWM/PWM.c
+++ Programmation/PWM/PWM.c
@@ -10,26 +10,27 @@ void init_PWM () {
 	// Déclaration du PORTD7 en sortie
 	DDRD |= (1 << PORTD7);
 
-	// Clear on up-counting and set on down-counting 
+	// Mise à 0 lorsque le compteur monte, mise à 1 lorsque le compteur descend
 	TCCR4C |= (1 << COM4D1) | (1 << PWM4D);
 
 	// Phase and Frequency Correct PWM
 	TCCR4D |= (1 << WGM40);
 
-	// Frequency
+	// Sélection de la fréquence
 	// 1024
 	TC4H = 0x03;
 	OCR4C = 0xFF;
-	// Duty cycle
+	// Sélection du rapport de cycle (duty cycle)
 	// 512
 	TC4H = 0x01;
 	OCR4D = 0xFF;
 
-	// Start timer (prescaler = 1)
+	// Activation du timer (prescaler = 1)
 	TCCR4B |= (1 << CS40);
 }
 
 void set_PWM_duty_cycle (short duty_cycle) {
+	// Change le rapport de cycle
 	TC4H = (((duty_cycle) & 0x300) >> 8);
 	OCR4D = (duty_cycle & 0xFF);
 }
@@ -46,7 +47,9 @@ int main(void) {
 	while (1) {
 		set_PWM_duty_cycle(i);
 
+		// Fait varier le rapport de cycle pour vérifier le fonctionnement
 		if ((i == 0) | (i == 1023)) {
+			// Arrêt plus long pour un rapport de cycle de 0% et 100%
 			_delay_ms(5000);
 		}
 		else {
